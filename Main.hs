c196446ecef3cbd212a3a2121ede1141b2d564ef
Christian Ouwehand <palaga@gmail.com>
2014-01-24 19:56:28 +0100

    Refactored: Added format function

--- Main.hs
+++ Main.hs
@@ -8,23 +8,25 @@ import Control.Monad                 (mapM_)
 import Text.Regex.PCRE
 
 
-type Fields = [(String, String)]
-type Filter = (String, String)
+type Fields       = [(String, String)]
+type Filter       = (String, String)
+type RefFormatter = [Filter] -> Reference -> [String]
 
 main = do
-  args        <- getArgs
-  let file    =  head args
-      filters =  pairElements $ tail args
-  references  <- parseFromFile bibliography file
-  case references of
-    Left  err  -> print err
-    Right refs -> mapM_ printReference $ refs `refMatching` filters
+    args          <- getArgs
+    let file      =  head args
+        filters   =  pairElements $ tail args
+    references    <- parseFromFile bibliography file
+    case references of
+      Left  err  -> print err
+      Right refs -> mapM_ (printReference filters defaultFormatter)
+                  $ refs `refMatching` filters
 
 
 refMatching :: Bibliography -> [Filter] -> Bibliography
 refMatching bib fs       = filter (predicate . getFields) bib
   where
-    predicate   r        = and $ map (matchFields r) fs
+    predicate   r        = and   $ map (matchFields r) fs
     matchCaseless x      = match $ makeRegexOpts compCaseless defaultExecOpt x
     matchFields r (k, p) = maybe False (matchCaseless p)
                          $ lookup k r
@@ -37,19 +39,19 @@ pairElements xs = case xs of
   _             -> []
 
 
-printReference :: Reference -> IO ()
-printReference r = do
-  putStrLn    $ colorize Green $ getName r ++ ":"
-  printFields $ getFields r
+printReference :: [Filter] -> RefFormatter -> Reference -> IO ()
+printReference filters formatter = mapM_ putStrLn . (formatter filters)
+
+
+defaultFormatter :: RefFormatter
+defaultFormatter fs r    = key : fields
+  where key              = colorize Green          $ getName r ++ ":"
+        fields           = map      fieldFormatter $ getFields r
+        fieldFormatter f = "  " ++ (colorize Red $ fst f ++ ": ") ++ snd f
 
 
 colorize :: Color -> String -> String
 colorize c s = colorCode ++ s ++ reset
   where colorCode = setSGRCode [SetColor Foreground Vivid c]
         reset     = setSGRCode [Reset]
-
-
-printFields :: Fields -> IO ()
-printFields fs = mapM_ printField fs
-  where printField f = putStrLn ("  " ++ fst f ++ ": " ++ snd f)
 
